5b70a2aa4736a8f3f95251d37b8260acb836e1c4
Dzianis <iVedzmar@gmail.com>
2015-08-11 00:12:29 +0300

    added creating workspace

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,42 +1,28 @@
 import com.epam.trainings.jcr.dao.EmployeeDAO;
 import com.epam.trainings.jcr.dao.impl.EmployeeDAOImpl;
 import com.epam.trainings.jcr.entities.Employee;
+import org.apache.jackrabbit.commons.JcrUtils;
 import org.apache.jackrabbit.core.TransientRepository;
 
 import javax.jcr.*;
 import java.io.File;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Iterator;
 import java.util.List;
 
 public class Main {
     private static final String JCR_DEFAULT_DIRECTORY = "C:/jcr_dir";
+    public static final String WORKSPACE_NAME = "myWorkspace";
 
-    public static void main(String[] args)  {
-        Repository repository = new TransientRepository(getRepositoryDir());
+    public static void main(String[] args) throws RepositoryException {
+        Repository repository = getRepository();
 
         Session session = null;
         try{
-            session = repository.login(
-                    new SimpleCredentials("admin","admin".toCharArray()));
-            printNodeTreeRecursive(session.getRootNode(), 0);
-            EmployeeDAO dao = new EmployeeDAOImpl(session);
-            
-            p("Number of employees : " + dao.getEmployees().size());
-            
-            for (Employee employee : dao.getEmployees()){
-                if (dao.deleteEmployee(employee)){
-                    
-                 p("An employee have been deleted");
-                }
-            }
+            session = getSession(repository);
 
-            dao.saveEmployee(new Employee("Dzanis", 25, Calendar.getInstance()));
 
-            List<Employee> employees = dao.getEmployees();
-            
-            p(employees);
-            
         } catch (RepositoryException e) {
             e.printStackTrace();
         } finally {
@@ -44,21 +30,47 @@ public class Main {
         }
     }
 
+    private static Repository getRepository() {
+        return new TransientRepository(getRepositoryDir());
+    }
+/*
+    private static Repository getRepository() throws RepositoryException {
+        return JcrUtils.getRepository("http://localhost:4502/crx/repository");
+    }
+*/
+    private static Session getSession(Repository repository) throws RepositoryException {
+        Session session = null;
+
+        try {
+            session = repository.login(
+                    new SimpleCredentials("admin","admin".toCharArray()));
+
+            if (Arrays.asList(session.getWorkspace().getAccessibleWorkspaceNames()).indexOf(WORKSPACE_NAME) == -1 ) {
+                session.getWorkspace().createWorkspace(WORKSPACE_NAME);
+            }
+
+        } finally {
+            if (session != null && session.isLive()) session.logout();
+        }
+
+        return repository.login(new SimpleCredentials("admin","admin".toCharArray()), WORKSPACE_NAME);
+    }
+
     public static void printNodeTreeRecursive(Node node, int depth) throws RepositoryException {
         Iterator<Node> iter = node.getNodes();
         for (int i = 0; i < depth; i++) {
             System.out.print("   ");
         }
-     
+
         p(node.getName());
         while (iter.hasNext()) printNodeTreeRecursive(iter.next(), depth + 1);
     }
-    
+
     private static File getRepositoryDir(){
         File file = new File(JCR_DEFAULT_DIRECTORY);
-        
+
         if (!file.exists()) file.mkdir();
-        
+
         return file;
     }
     
